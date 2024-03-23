3f59739f90bdae57d335c222f4c6e68c5109c846
mohaiminul69 <mohaiminul69@gmail.com>
2024-03-23 17:23:27 +0600

    M updated.

--- week_2/day_2/kalindrome_array.cpp
+++ week_2/day_2/kalindrome_array.cpp
@@ -1,7 +1,6 @@
 // https://codeforces.com/problemset/problem/1610/B
 
 #include <bits/stdc++.h>
-#define ll long long int
 using namespace std;
 
 bool isPalindrome(const vector<int> &v, int valueToSkip)
