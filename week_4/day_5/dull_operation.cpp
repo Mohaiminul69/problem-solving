22fd110ca9000da2d52180b7da7b832bf849999d
mohaiminul69 <mohaiminul69@gmail.com>
2024-04-13 12:38:16 +0600

    n done.

---
+++ week_4/day_5/dull_operation.cpp
@@ -0,0 +1,17 @@
+// https://www.codechef.com/problems/DUPLET
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n, cout << n << " " << n - 1 << '\n';
+    }
+
+    return 0;
+}
