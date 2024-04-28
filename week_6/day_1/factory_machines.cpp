6f17e40ee3cb83448db5e71da9b4e2fb0df65641
mohaiminul69 <mohaiminul69@gmail.com>
2024-04-28 22:02:27 +0600

    k done.

---
+++ week_6/day_1/factory_machines.cpp
@@ -0,0 +1,41 @@
+// https://cses.fi/problemset/task/1620
+
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    int n, t;
+    cin >> n >> t;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+
+    auto ok = [&](ll m)
+    {
+        ll total = 0;
+        for (int i = 0; i < n; i++)
+        {
+            total += m / v[i];
+            if (total >= t)
+                return true;
+        }
+        return false;
+    };
+
+    ll l = 1, r = 1e18, mid, ans;
+
+    while (l <= r)
+    {
+        mid = (l + r) / 2;
+        if (ok(mid))
+            ans = mid, r = mid - 1;
+        else
+            l = mid + 1;
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
